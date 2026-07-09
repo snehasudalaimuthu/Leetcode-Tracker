dec2490124361342421b097684608d508a7bae05
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 11:19:55 +0530

    Create 67-AddBinary

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/67-AddBinary/67-AddBinary.java
@@ -0,0 +1,23 @@
+// Last updated: 09/07/2026, 11:19:55
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int d1 = (i >= 0) ? a.charAt(i) - '0' : 0;
+            int d2 = (j >= 0) ? b.charAt(j) - '0' : 0;
+            int sum = d1 + d2 + carry;
+            
+            sb.append(sum % 2);   
+            carry = sum / 2;      
+            
+            i--;
+            j--;
+        }
+
+        return sb.reverse().toString();
+    }
+}
