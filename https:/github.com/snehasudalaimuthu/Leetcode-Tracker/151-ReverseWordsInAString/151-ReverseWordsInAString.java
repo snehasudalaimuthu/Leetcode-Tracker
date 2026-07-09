cb52a23c791082e5eac80766a5a0e1164b37cea3
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 11:18:50 +0530

    Create 151-ReverseWordsInAString

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/151-ReverseWordsInAString/151-ReverseWordsInAString.java
@@ -0,0 +1,19 @@
+// Last updated: 09/07/2026, 11:18:50
+class Solution {
+    public String reverseWords(String s) {
+        // Trim the input string to remove leading and trailing spaces
+        String[] str = s.trim().split("\\s+");
+
+        // Initialize the output string
+        String out = "";
+
+        // Iterate through the words in reverse order
+        for (int i = str.length - 1; i > 0; i--) {
+            // Append the current word and a space to the output
+            out += str[i] + " ";
+        }
+
+        // Append the first word to the output (without trailing space)
+        return out + str[0];
+    }
+}
