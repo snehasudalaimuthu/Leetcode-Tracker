c69a828bc4041c0ab4f04bd3226c65ed2717e997
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 11:20:17 +0530

    Create 48-RotateImage

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/48-RotateImage/48-RotateImage.java
@@ -0,0 +1,27 @@
+// Last updated: 09/07/2026, 11:20:17
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
