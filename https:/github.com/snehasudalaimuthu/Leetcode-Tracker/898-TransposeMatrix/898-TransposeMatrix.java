185f53b661c0e879cb46ecac9a451c27a4c993a5
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 09:47:16 +0530

    Create 898-TransposeMatrix

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/898-TransposeMatrix/898-TransposeMatrix.java
@@ -0,0 +1,12 @@
+// Last updated: 09/07/2026, 09:47:16
+class Solution {
+    public int[][] transpose(int[][] A) {
+        int R = A.length, C = A[0].length;
+        int[][] ans = new int[C][R];
+        for (int r = 0; r < R; ++r)
+            for (int c = 0; c < C; ++c) {
+                ans[c][r] = A[r][c];
+            }
+        return ans;
+    }
+}
