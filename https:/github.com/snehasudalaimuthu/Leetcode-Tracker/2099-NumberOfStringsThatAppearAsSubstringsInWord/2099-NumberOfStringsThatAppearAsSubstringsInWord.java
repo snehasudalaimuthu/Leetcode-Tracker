3a5ce10aa3002616f9086d796e3726b8c270d4d0
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 10:03:30 +0530

    Create 2099-NumberOfStringsThatAppearAsSubstringsInWord

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/2099-NumberOfStringsThatAppearAsSubstringsInWord/2099-NumberOfStringsThatAppearAsSubstringsInWord.java
@@ -0,0 +1,12 @@
+// Last updated: 09/07/2026, 10:03:30
+class Solution {
+    public int numOfStrings(String[] patterns, String word) {
+        int count=0;
+        for(int i=0;i<patterns.length;i++){
+            if(word.contains(patterns[i])){
+                count++;
+            }
+        }
+        return count;
+    }
+}
