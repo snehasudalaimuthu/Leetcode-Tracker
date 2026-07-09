c3f0d2c294844d574786c9d57883151062c36509
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 09:47:34 +0530

    Create 717-1BitAnd2BitCharacters

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/717-1BitAnd2BitCharacters/717-1BitAnd2BitCharacters.java
@@ -0,0 +1,11 @@
+// Last updated: 09/07/2026, 09:47:34
+class Solution {
+
+    public boolean isOneBitCharacter(int[] bits) {
+        int i = 0;
+        while (i < bits.length - 1) {
+            i += bits[i] + 1;
+        }
+        return i == bits.length - 1;
+    }
+}
