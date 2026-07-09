c4e66fe9b72ff62fffb324955a7d1bdbee162ab3
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 11:20:35 +0530

    Create 35-SearchInsertPosition

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -0,0 +1,14 @@
+// Last updated: 09/07/2026, 11:20:35
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+            if (nums[i] > target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
