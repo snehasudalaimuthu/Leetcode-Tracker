66ee20aade0c6ddb2ffa5ba5ac49f83f189c0f87
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 11:20:55 +0530

    Create 22-GenerateParentheses

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/22-GenerateParentheses/22-GenerateParentheses.java
@@ -0,0 +1,25 @@
+// Last updated: 09/07/2026, 11:20:55
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+
+        dfs(0, 0, "", n, res);
+
+        return res;        
+    }
+
+    private void dfs(int openP, int closeP, String s, int n, List<String> res) {
+        if (openP == closeP && openP + closeP == n * 2) {
+            res.add(s);
+            return;
+        }
+
+        if (openP < n) {
+            dfs(openP + 1, closeP, s + "(", n, res);
+        }
+
+        if (closeP < openP) {
+            dfs(openP, closeP + 1, s + ")", n, res);
+        }
+    }    
+}
