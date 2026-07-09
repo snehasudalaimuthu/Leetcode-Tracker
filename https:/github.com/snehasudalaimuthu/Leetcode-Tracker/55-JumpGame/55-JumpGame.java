7cf10a7e3387bec3a5130dacefdf053ddf4bcd7e
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 11:20:08 +0530

    Create 55-JumpGame

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/55-JumpGame/55-JumpGame.java
@@ -0,0 +1,11 @@
+// Last updated: 09/07/2026, 11:20:08
+class Solution {
+    public boolean canJump(int[] nums) {
+       int reachable = 0;
+       for(int i = 0; i < nums.length; i ++) {
+           if(i > reachable) return false;
+           reachable = Math.max(reachable, i + nums[i]);
+       } 
+       return true;
+    }
+}
