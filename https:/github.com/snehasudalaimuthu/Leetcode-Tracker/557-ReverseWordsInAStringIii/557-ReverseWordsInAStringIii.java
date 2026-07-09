7bcedefa9ab6fe5c7a36a6a01becbe1f7e2b46da
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 10:04:17 +0530

    Create 557-ReverseWordsInAStringIii

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/557-ReverseWordsInAStringIii/557-ReverseWordsInAStringIii.java
@@ -0,0 +1,16 @@
+// Last updated: 09/07/2026, 10:04:17
+class Solution {
+    public String reverseWords(String s) {
+        // Split the input string into words and reverse each word
+        String[] words = s.split(" ");
+        StringBuilder result = new StringBuilder();
+        for (String word : words) {
+            StringBuilder reversedWord = new StringBuilder(word).reverse();
+            result.append(reversedWord).append(" ");
+        }
+        result.deleteCharAt(result.length() - 1);  // Remove the extra space at the end
+        
+        // Convert StringBuilder to String
+        return result.toString();        
+    }
+}
