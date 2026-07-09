0762f348a15e773b8facfd79b43d2e46b9987df8
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 11:19:20 +0530

    Create 106-ConstructBinaryTreeFromInorderAndPostorderTraversal

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/106-ConstructBinaryTreeFromInorderAndPostorderTraversal/106-ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -0,0 +1,47 @@
+// Last updated: 09/07/2026, 11:19:20
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    int postIndex;
+    Map<Integer, Integer> map = new HashMap<>();
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+
+        postIndex = postorder.length - 1;
+
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+
+        return build(inorder, postorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode build(int[] inorder, int[] postorder, int left, int right) {
+
+        if (left > right) return null;
+
+        int rootVal = postorder[postIndex--];
+        TreeNode root = new TreeNode(rootVal);
+
+        int index = map.get(rootVal);
+
+        root.right = build(inorder, postorder, index + 1, right);
+        root.left = build(inorder, postorder, left, index - 1);
+
+        return root;
+    }
+}
