c5380bcf88c764e3e7e6cc29b5fdcf8192f468d7
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 09:47:35 +0530

    Create 713-SubarrayProductLessThanK

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.java
@@ -0,0 +1,24 @@
+// Last updated: 09/07/2026, 09:47:35
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1)
+            return 0;
+
+        int left = 0;
+        int product = 1;
+        int count = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            product *= nums[right];
+
+            while (product >= k) {
+                product /= nums[left];
+                left++;
+            }
+
+            count += right - left + 1;
+        }
+
+        return count;
+    }
+}
