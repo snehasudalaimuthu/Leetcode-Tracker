f6315ffef1f77ccd5868a2c4873f23079acace0d
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 09:46:35 +0530

    Create 4057-TotalWavinessOfNumbersInRangeI

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/4057-TotalWavinessOfNumbersInRangeI/4057-TotalWavinessOfNumbersInRangeI.java
@@ -0,0 +1,21 @@
+// Last updated: 09/07/2026, 09:46:35
+class Solution {
+    public int totalWaviness(int num1, int num2) {
+        int ans = 0;
+
+        for (int x = num1; x <= num2; x++) {
+            String s = String.valueOf(x);
+
+            for (int i = 1; i < s.length() - 1; i++) {
+                if ((s.charAt(i) > s.charAt(i - 1) &&
+                     s.charAt(i) > s.charAt(i + 1)) ||
+                    (s.charAt(i) < s.charAt(i - 1) &&
+                     s.charAt(i) < s.charAt(i + 1))) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
