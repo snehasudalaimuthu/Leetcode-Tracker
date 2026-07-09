39169d001ba4223e00bc18267c3b83e666c5b1df
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 11:18:35 +0530

    Create 171-ExcelSheetColumnNumber

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/171-ExcelSheetColumnNumber/171-ExcelSheetColumnNumber.java
@@ -0,0 +1,14 @@
+// Last updated: 09/07/2026, 11:18:35
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        long ans = 0;
+
+        // Same idea as decimal parsing, but base is 26.
+        for (int i = 0; i < columnTitle.length(); i++) {
+            char ch = columnTitle.charAt(i);
+            ans = ans * 26 + (ch - 'A' + 1);
+        }
+
+        return (int) ans;
+    }
+}
