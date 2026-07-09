cc687d2b2c4c6087c06361b6b64951cf701c7c08
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 10:04:35 +0530

    Create 448-FindAllNumbersDisappearedInAnArray

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/448-FindAllNumbersDisappearedInAnArray/448-FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,19 @@
+// Last updated: 09/07/2026, 10:04:35
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int idx = Math.abs(nums[i]) - 1;
+            if (nums[idx] > 0) {
+                nums[idx] *= -1;
+            }
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+        return result;        
+    }
+}
