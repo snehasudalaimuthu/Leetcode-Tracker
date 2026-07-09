12cce2c21713d356bf8a8b97b2007e63d3fcb2eb
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 09:47:32 +0530

    Create 724-FindPivotIndex

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/724-FindPivotIndex/724-FindPivotIndex.java
@@ -0,0 +1,18 @@
+// Last updated: 09/07/2026, 09:47:32
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0;
+        for(int num : nums){
+            totalSum += num;
+        }
+        int leftSum = 0;
+        for(int i = 0; i < nums.length; i++){
+            int rightSum = totalSum - leftSum - nums[i];
+            if(leftSum == rightSum){
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+}
