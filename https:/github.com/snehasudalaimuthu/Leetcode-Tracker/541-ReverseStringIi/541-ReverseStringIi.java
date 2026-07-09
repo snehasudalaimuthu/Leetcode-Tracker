f9ecaabf9477f927c512ea3f82d53723fab69771
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 10:04:20 +0530

    Create 541-ReverseStringIi

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/541-ReverseStringIi/541-ReverseStringIi.java
@@ -0,0 +1,28 @@
+// Last updated: 09/07/2026, 10:04:20
+class Solution {
+    public void reverse(char[] arr , int i , int j) {
+        while(i <= j) {
+            char t = arr[i];
+            arr[i] = arr[j];
+            arr[j] = t;
+
+            i++;
+            j--;
+        }
+    }
+
+    public String reverseStr(String s, int k) {
+        char[] arr = s.toCharArray();
+        int n = s.length();
+
+        int i = 0;
+        while(i < n) {
+            int j = Math.min(i + k - 1 , n - 1);
+            reverse(arr , i , j);
+
+            i += 2 * k;
+        }
+        
+        return new String(arr);
+    }
+}
