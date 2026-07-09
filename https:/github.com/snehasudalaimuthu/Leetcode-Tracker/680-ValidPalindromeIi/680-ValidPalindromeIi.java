f696db6f6da0bc6a0b1cfc100cc67687d808bf35
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 09:47:38 +0530

    Create 680-ValidPalindromeIi

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/680-ValidPalindromeIi/680-ValidPalindromeIi.java
@@ -0,0 +1,26 @@
+// Last updated: 09/07/2026, 09:47:38
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        
+        while(i <= j){
+            if(s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+            }
+            else return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+        }
+        return true;
+    }
+    public boolean isPalindrome(String s, int i, int j){
+        while(i <= j){
+            if(s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+            }
+            else return false;
+        }
+        return true;
+    }
+}
