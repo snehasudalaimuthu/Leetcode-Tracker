07347b7e62b43c8e92c1084bdec9b667111fbb23
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 09:46:36 +0530

    Create 4135-ConcatenateNonZeroDigitsAndMultiplyBySumI

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/4135-ConcatenateNonZeroDigitsAndMultiplyBySumI/4135-ConcatenateNonZeroDigitsAndMultiplyBySumI.java
@@ -0,0 +1,12 @@
+// Last updated: 09/07/2026, 09:46:36
+class Solution {
+    public long sumAndMultiply(int n) {
+        long x = 0, s = 0;
+        for (char c : String.valueOf(n).toCharArray())
+            if (c != '0') {
+                x = x * 10 + c - '0';
+                s += c - '0';
+            }
+        return x * s;
+    }
+}
