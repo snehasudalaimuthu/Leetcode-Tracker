bf91cc73b62848bb994872b6a933a327787f44ef
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 11:19:41 +0530

    Create 78-Subsets

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/78-Subsets/78-Subsets.java
@@ -0,0 +1,18 @@
+// Last updated: 09/07/2026, 11:19:41
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        for(int num : nums){
+            int n = outer.size();
+            for(int i = 0; i < n; i++){
+                // Create a new subset from existing one
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal); // Add it to the list
+            }
+        }
+        return outer;
+    }
+}
