22eb1d1b33947dc4e676885660ed3e513dc2382e
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 09:47:13 +0530

    Create 1013-FibonacciNumber

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/1013-FibonacciNumber/1013-FibonacciNumber.java
@@ -0,0 +1,16 @@
+// Last updated: 09/07/2026, 09:47:13
+class Solution {
+    public int fib(int n) {
+        if(n==0){
+            return 0;
+        }
+        int a=0;
+        int b=1;
+        for(int i=2;i<=n;i++){
+            int c=a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+}
