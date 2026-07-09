30b4c1a17dfd42d417a751912d5fbbad69c65462
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 10:04:08 +0530

    Create 611-ValidTriangleNumber

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/611-ValidTriangleNumber/611-ValidTriangleNumber.java
@@ -0,0 +1,26 @@
+// Last updated: 09/07/2026, 10:04:08
+import java.util.Arrays;
+
+class Solution {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int count = 0;
+        int n = nums.length;
+
+        for (int k = n - 1; k >= 2; k--) {
+            int i = 0;
+            int j = k - 1;
+
+            while (i < j) {
+                if (nums[i] + nums[j] > nums[k]) {
+                    count += (j - i);
+                    j--;
+                } else {
+                    i++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
