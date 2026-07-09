3b0a19509fe49845c3f0954d7d813f40041d534e
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 11:19:02 +0530

    Create 134-GasStation

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/134-GasStation/134-GasStation.java
@@ -0,0 +1,19 @@
+// Last updated: 09/07/2026, 11:19:02
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int sGas = 0, sCost = 0, res = 0, total = 0;
+        for (int i = 0; i < gas.length; i++) {
+            sGas += gas[i];
+            sCost += cost[i];
+        }
+        if (sGas < sCost) return -1;
+        for (int i = 0; i < gas.length; i++) {
+            total += gas[i] - cost[i];
+            if (total < 0) {
+                total = 0;
+                res = i + 1;
+            }
+        }
+        return res;
+    }
+}
