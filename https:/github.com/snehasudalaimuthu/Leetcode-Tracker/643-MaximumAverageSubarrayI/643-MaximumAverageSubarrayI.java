71df3fdaa79f2926892483a2eb7d5302c3089346
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 10:04:05 +0530

    Create 643-MaximumAverageSubarrayI

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.java
@@ -0,0 +1,22 @@
+// Last updated: 09/07/2026, 10:04:05
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int windowSum = 0;
+
+        // first window
+        for (int i = 0; i < k; i++) {
+            windowSum += nums[i];
+        }
+
+        int maxSum = windowSum;
+
+        // slide the window
+        for (int i = k; i < nums.length; i++) {
+            windowSum += nums[i];        // add new element
+            windowSum -= nums[i - k];    // remove old element
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        return (double) maxSum / k;
+    }
+}
