118b9156be9f8d9802735357680edbd0f9a5776b
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-09 11:20:45 +0530

    Create 28-FindTheIndexOfTheFirstOccurrenceInAString

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/28-FindTheIndexOfTheFirstOccurrenceInAString/28-FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,7 @@
+// Last updated: 09/07/2026, 11:20:45
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int index = haystack.indexOf(needle);
+        return index;
+    }
+}
