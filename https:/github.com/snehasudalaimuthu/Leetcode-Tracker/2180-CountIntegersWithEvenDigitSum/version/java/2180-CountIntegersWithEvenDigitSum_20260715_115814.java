35ee27bbb2db76b935e403d309d447d0cd7f0679
snehasudalaimuthu <snehamalar1513@gmail.com>
2026-07-15 11:58:15 +0530

    Create 2180-CountIntegersWithEvenDigitSum

---
+++ https:/github.com/snehasudalaimuthu/Leetcode-Tracker/2180-CountIntegersWithEvenDigitSum/version/java/2180-CountIntegersWithEvenDigitSum_20260715_115814.java
@@ -0,0 +1,18 @@
+// Last updated: 7/15/2026, 11:58:14 AM
+1class Solution {
+2    public int countEven(int num) {
+3      int count=0;
+4      for(int i=1;i<=num;i++){
+5        int sum=0;
+6        int temp=i;
+7        while(temp>0){
+8            sum=sum+temp%10;
+9            temp=temp/10;
+10        }
+11        if(sum%2==0){
+12            count++;
+13        }
+14      }
+15      return count;
+16    }
+17}
